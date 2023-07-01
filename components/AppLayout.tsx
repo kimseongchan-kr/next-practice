dc266562746a1642c10c3d5724b82cd59a53993e
chan <fpel8800@gmail.com>
2023-07-01 20:40:08 +0900

    fix: login dummy data 작업
    - Input.Search 컴포넌트 inline-style => styled-component로 변경

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,19 +1,19 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useState } from 'react';
 import Link from 'next/link';
 import { Col, Input, Menu, Row } from 'antd';
 import { getItem } from '@utils/getItem';
+import UserProfile from './UserProfile';
+import LoginForm from './LoginForm';
+import styled from 'styled-components';
 
 const AppLayout = ({ children }: PropsWithChildren<any>) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   const item = [
     getItem(<Link href="/">노드버드</Link>),
     getItem(<Link href="/profile">프로필</Link>),
     getItem(<Link href="/signup">회원가입</Link>),
-    getItem(
-      <Input.Search
-        placeholder="input search text"
-        style={{ width: 200, verticalAlign: 'middle' }}
-      />
-    ),
+    getItem(<SearchInput placeholder="input search text" />),
   ];
 
   return (
@@ -21,7 +21,7 @@ const AppLayout = ({ children }: PropsWithChildren<any>) => {
       <Menu theme="dark" mode="horizontal" items={item}></Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽 메뉴래
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
@@ -39,5 +39,10 @@ const AppLayout = ({ children }: PropsWithChildren<any>) => {
     </div>
   );
 };
+
+const SearchInput = styled(Input.Search)`
+  width: 200px;
+  vertical-align: middle;
+`;
 
 export default AppLayout;
